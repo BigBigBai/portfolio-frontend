246140a42e420f0030097374da5d7b122aa51e4a
BigBigBai <baiyingjian@outlook.com>
2025-10-31 15:20:09 -0400

    fetch projects from strapi

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -9,10 +9,31 @@ export async function loader({
   request,
 }: Route.LoaderArgs): Promise<{ projects: Project[] }> {
   // const res = await fetch('http://localhost:8000/projects');
-  const res = await fetch(`${import.meta.env.VITE_API_URL}/projects`);
-  const data = await res.json();
+  const res = await fetch(
+    `${import.meta.env.VITE_API_URL}/projects?populate=*`
+  );
 
-  return { projects: data };
+  if (!res.ok) {
+    throw new Error('Failed to fetch projects');
+  }
+
+  const json = await res.json();
+
+  console.log(json);
+
+  const projects = json.data.map((item) => ({
+    id: item.id,
+    documentId: item.documentId,
+    title: item.title,
+    description: item.description,
+    image: item.image?.url
+      ? `${import.meta.env.VITE_API_URL}${item.image.url}`
+      : '/images/no-image.png',
+    category: item.category,
+    featured: item.featured,
+  }));
+
+  return { projects };
 }
 
 const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
