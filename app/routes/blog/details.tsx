9737a5109d3bbb98645cb81b8702e70f9558852a
BigBigBai <baiyingjian@outlook.com>
2025-10-31 21:08:51 -0400

    fetch individual post

--- app/routes/blog/details.tsx
+++ app/routes/blog/details.tsx
@@ -2,56 +2,85 @@ import ReactMarkdown from 'react-markdown';
 import type { Route } from './+types/details';
 import type { PostMeta } from '~/types';
 import { Link } from 'react-router';
+import type { StrapiResponse, StrapiPost } from '~/types';
 
 export async function loader({ request, params }: Route.LoaderArgs) {
   const { slug } = params;
   //   console.log(slug);
   //   console.log('Request URL:', request.url.toString());
 
-  const url = new URL('/posts-meta.json', request.url);
+  // const url = new URL('/posts-meta.json', request.url);
   //   console.log(url.toString());
+  // const res = await fetch(url.href);
 
-  const res = await fetch(url.href);
+  const res = await fetch(
+    `${import.meta.env.VITE_API_URL}/api/posts?filters[slug][$eq]=${slug}&populate=image`
+  );
+
   if (!res.ok) {
     throw new Error('Failed to fetch posts');
   }
 
-  const index = await res.json();
-  const postMeta = index.find((post: PostMeta) => post.slug === slug);
-  if (!postMeta) {
+  const json: StrapiResponse<StrapiPost> = await res.json();
+
+  if (!json.data.length) {
     throw new Response('Not Found', { status: 404 });
   }
 
-  const markdown = await import(`../../posts/${slug}.md?raw`);
+  const item = json.data[0];
 
-  return {
-    postMeta,
-    markdown: markdown.default,
+  const post: PostMeta = {
+    id: item.id,
+    slug: item.slug,
+    title: item.title,
+    excerpt: item.excerpt,
+    date: item.date,
+    image: item.image?.url
+      ? `${import.meta.env.VITE_API_URL}${item.image.url}`
+      : '/images/no-image.png',
   };
+
+  return { post };
+
+  // const index = await res.json();
+  // const postMeta = index.find((post: PostMeta) => post.slug === slug);
+  // if (!postMeta) {
+  //   throw new Response('Not Found', { status: 404 });
+  // }
+  // const markdown = await import(`../../posts/${slug}.md?raw`);
+  // return {
+  //   postMeta,
+  //   markdown: markdown.default,
+  // };
 }
 
-type BlogPostDetailsPageProps = {
-  loaderData: {
-    postMeta: PostMeta;
-    markdown: string;
-  };
-};
+// type BlogPostDetailsPageProps = {
+//   loaderData: {
+//     postMeta: PostMeta;
+//     markdown: string;
+//   };
+// };
 
-const BlogPostDetailsPage = ({ loaderData }: BlogPostDetailsPageProps) => {
-  const { postMeta, markdown } = loaderData;
+const BlogPostDetailsPage = ({ loaderData }: Route.ComponentProps) => {
+  const { post } = loaderData;
   //   console.log(postMeta, markdown);
 
   return (
     <div className='max-w-3xl mx-auto px-6 py-12 bg-gray-900'>
-      <h1 className='text-3xl font-bold text-blue-400 mb-2'>
-        {postMeta.title}
-      </h1>
+      <h1 className='text-3xl font-bold text-blue-400 mb-2'>{post.title}</h1>
       <p className='text-sm text-gray-400 mb-6'>
-        {new Date(postMeta.date).toLocaleDateString()}
+        {new Date(post.date).toLocaleDateString()}
       </p>
 
+      <img
+        src={post.image}
+        alt={post.title}
+        className='w-full h-64 object-cover mb-4'
+      />
+
       <div className='prose prose-invert max-w-none mb-12'>
-        <ReactMarkdown>{markdown}</ReactMarkdown>
+        {/* <ReactMarkdown>{markdown}</ReactMarkdown> */}
+        The Body
       </div>
 
       <div className='text-center'>
